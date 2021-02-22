038ecf05f6d9dcc20fe737319d8fdde7e88d09fb
HarrySanchez-AES-2020 <sanchezharry@javeriana.edu.co>
2021-02-22 09:00:07 -0500

    Project setup

---
+++ src/main/java/co/com/mutant/recognition/RecognitionApplication.java
@@ -0,0 +1,13 @@
+package co.com.mutant.recognition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecognitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecognitionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/co/com/mutant/recognition/RecognitionApplicationTests.java
@@ -0,0 +1,13 @@
+package co.com.mutant.recognition;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RecognitionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
